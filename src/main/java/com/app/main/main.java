86f9ddeaba7ebb152c0f8a87d6f8012a238b8831
KoichiSugi <43499213+KoichiSugi@users.noreply.github.com>
2021-05-30 21:46:01 +1000

    deserialization
    deserialization completed

--- src/main/java/com/app/main/main.java
+++ src/main/java/com/app/main/main.java
@@ -1,10 +1,10 @@
 package com.app.main;
 
 import com.app.user.Rows;
-import com.app.user.Users;
 import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,19 +24,38 @@ class Main {
         mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
 
         try {
+            // Convert JSON string from file to Object by providing file
+           // ArrayList<Rows> rowsArrayList = mapper.readValue(new File(ClientsRecordsPath), ArrayList.class);
+            Rows test = mapper.readValue(new File(ClientsRecordsPath), Rows.class);
 
-            // Convert JSON string from file to Object
-//            Rows row = mapper.readValue(new File(ClientsRecordsPath), Rows.class);
+            //json array to array object00
+            System.out.println("JSON array to Array objects...");
+           // System.out.println(rowsArrayList.get(0));
+            System.out.println(test.getRows().get(1).getUserId());
+            //print a single row
+            System.out.println(test.getRows().get(1).toString());
+            System.out.println(test.getRows().size());
+            //print all the rows
+            for (int i =0; i<test.getRows().size(); i++){
+                System.out.println(test.getRows().get(i).toString());
+            }
 
-            //Collection<Users> UserList = mapper.readValue(new File(ClientsRecordsPath), Collection.class);
-            List<Users> UserList = mapper.readValue(new File(ClientsRecordsPath), ArrayList.class);
-            List<Users> UserList2 = mapper.readValue(new File(ClientsRecordsPath), new TypeReference<List<Users>>() {});
-            //exp
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            TypeFactory typeFactory = objectMapper.getTypeFactory();
-//            List<Users> UserList = objectMapper.readValue(ClientsRecordsPath, typeFactory.constructCollectionType(List.class, Users.class));
+            //System.out.println(test);//this will print everything
 
-            System.out.println(UserList2.size());
+//            for (Rows rows : rowsArray) {
+//                System.out.println(rows);
+//            }
+
+
+//            List<Users> UserList = mapper.readValue(new File(ClientsRecordsPath), ArrayList.class);
+//            List<Row> rowList = mapper.readValue(new File(ClientsRecordsPath), ArrayList.class);
+//            //exp
+//            //Row[] myobjects = mapper.readValue(ClientsRecordsPath, Row[].class);
+//            List<Row> myObjects = Arrays.asList(mapper.readValue(ClientsRecordsPath, Row[].class));
+//
+//
+//            //System.out.println(rowList.size());
+//            System.out.println(myObjects);
 
 //            }
 
---
+++ src/main/java/com/app/user/Row.java
@@ -0,0 +1,113 @@
+package com.app.user;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import javax.annotation.Generated;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "USER_ID",
+        "COMMISSION",
+        "SWAPS",
+        "PROFIT",
+        "COMMENT"
+})
+@Generated("jsonschema2pojo")
+public class Row {
+    @JsonProperty("USER_ID")
+    private int userId;
+    @JsonProperty("COMMISSION")
+    private float commission;
+    @JsonProperty("SWAPS")
+    private float swaps;
+    @JsonProperty("PROFIT")
+    private float profit;
+    @JsonProperty("COMMENT")
+    private String comment;
+
+    @JsonProperty("USER_ID")
+    public int getUserId() {
+        return userId;
+    }
+
+    @JsonProperty("USER_ID")
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    @JsonProperty("COMMISSION")
+    public float getCommission() {
+        return commission;
+    }
+
+    @JsonProperty("COMMISSION")
+    public void setCommission(float commission) {
+        this.commission = commission;
+    }
+
+    @JsonProperty("SWAPS")
+    public float getSwaps() {
+        return swaps;
+    }
+
+    @JsonProperty("SWAPS")
+    public void setSwaps(float swaps) {
+        this.swaps = swaps;
+    }
+
+    @JsonProperty("PROFIT")
+    public float getProfit() {
+        return profit;
+    }
+
+    @JsonProperty("PROFIT")
+    public void setProfit(float profit) {
+        this.profit = profit;
+    }
+
+    @JsonProperty("COMMENT")
+    public String getComment() {
+        return comment;
+    }
+
+    @JsonProperty("COMMENT")
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Row.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("userId");
+        sb.append('=');
+        sb.append(this.userId);
+        sb.append(',');
+        sb.append("commission");
+        sb.append('=');
+        sb.append(this.commission);
+        sb.append(',');
+        sb.append("swaps");
+        sb.append('=');
+        sb.append(this.swaps);
+        sb.append(',');
+        sb.append("profit");
+        sb.append('=');
+        sb.append(this.profit);
+        sb.append(',');
+        sb.append("comment");
+        sb.append('=');
+        sb.append(((this.comment == null) ? "<null>" : this.comment));
+        sb.append(',');
+        if (sb.charAt((sb.length() - 1)) == ',') {
+            sb.setCharAt((sb.length() - 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
+
