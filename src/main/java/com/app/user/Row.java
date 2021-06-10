ccb6a15b1b2f439a7ef716780602b5598c504528
KoichiSugi <43499213+KoichiSugi@users.noreply.github.com>
2021-06-10 13:44:52 +1000

    Update Row.java

--- src/main/java/com/app/user/Row.java
+++ src/main/java/com/app/user/Row.java
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import javax.annotation.Generated;
-import java.util.List;
-
 /**
  * @Author Koichi Sugi
  */
